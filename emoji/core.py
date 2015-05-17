df031be88c0363de6df27d8961b39c14d8b7007a
Taehoon Kim <carpedm20@gmail.com>
2015-05-17 14:39:15 +0900

    emoji.decode() removed. #10

--- emoji/core.py
+++ emoji/core.py
@@ -45,31 +45,3 @@ def emojize(string, use_aliases=USE_ALIASES):
             return unicode_codes.EMOJI_UNICODE.get(match.group(1), match.group(1))
 
     return pattern.sub(replace, string)
-
-
-def decode(u_code, use_aliases=USE_ALIASES):
-
-    """Given a unicode code return the name of the associated emoji.
-
-    :param u_code : String contains unicode emoji.
-    :param use_aliases: (optional) Whether uses aliase of emoji name or not.
-    :raises ValueError: unrecognized string.
-    :return: Name of associated emoji.
-
-        >>> import emoji
-        >>> print(emoji.decode("👍"), use_aliases=True)
-        :+1:
-        >>> print(emoji.decode("👍"))
-        :thumbs_up_sign:
-    """
-
-    if PY2:  # pragma no cover
-        u_code = u_code.decode('utf-8')
-
-    try:
-        if use_aliases:
-            return unicode_codes.UNICODE_EMOJI_ALIAS[u_code]
-        else:
-            return unicode_codes.UNICODE_EMOJI[u_code]
-    except KeyError:
-        raise ValueError("Unicode code is not an emoji: %s" % u_code)
