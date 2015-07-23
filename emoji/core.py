3f6beaa144e4ed5cd6017683bc631b90ebd0942f
Brendan Adkins <brendan@developmentnow.com>
2015-07-23 13:05:42 -0500

    Taking a crack at issue #14.

--- emoji/core.py
+++ emoji/core.py
@@ -45,3 +45,56 @@ def emojize(string, use_aliases=USE_ALIASES):
             return unicode_codes.EMOJI_UNICODE.get(match.group(1), match.group(1))
 
     return pattern.sub(replace, string)
+
+def demojize(string):
+
+    """Replace unicode emoji in a string with emoji shortcodes. Useful for storage.
+
+    :param string: String contains unicode characters. MUST BE UNICODE.
+
+        >>> import emoji
+        >>> print(emoji.emojize("Python is fun :thumbs_up_sign:"))
+        Python is fun 👍
+        >>> print(emoji.demojize(u"Python is fun 👍"))
+        Python is fun :thumbs_up_sign:
+        >>> print(emoji.demojize("Unicode is tricky 😯".decode('utf-8')))
+        Python is fun :thumbs_up_sign:
+    """
+
+    # via Martijn Pieters, http://stackoverflow.com/questions/26568722/remove-unicode-emoji-using-re-in-python
+    if PY2:
+        try:
+            # Wide UCS-4 build
+            pattern = re.compile(u'['
+                u'\U0001F300-\U0001F64F'
+                u'\U0001F680-\U0001F6FF'
+                u'\u2600-\u26FF\u2700-\u27BF]+', 
+                re.UNICODE)
+        except re.error:
+            # Narrow UCS-2 build
+            pattern = re.compile(u'('
+                u'\ud83c[\udf00-\udfff]|'
+                u'\ud83d[\udc00-\ude4f\ude80-\udeff]|'
+                u'[\u2600-\u26FF\u2700-\u27BF])+', 
+                re.UNICODE)
+    else:
+    # Python 3+ should handle all strings as unicode. Right?
+        try:
+            # Wide UCS-4 build
+            pattern = re.compile(u'['
+                '\U0001F300-\U0001F64F'
+                '\U0001F680-\U0001F6FF'
+                '\u2600-\u26FF\u2700-\u27BF]+', 
+                re.UNICODE)
+        except re.error:
+            # Narrow UCS-2 build
+            pattern = re.compile(u'('
+                '\ud83c[\udf00-\udfff]|'
+                '\ud83d[\udc00-\ude4f\ude80-\udeff]|'
+                '[\u2600-\u26FF\u2700-\u27BF])+', 
+                re.UNICODE)
+
+    def replace(match):
+        return unicode_codes.UNICODE_EMOJI.get(match.group(1), match.group(1))
+
+    return pattern.sub(replace, string)
