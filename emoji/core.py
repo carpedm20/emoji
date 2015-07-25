7ffd32e61b78880085f3830e5b4ff1ab4ed8caef
Brendan Adkins <brendan@developmentnow.com>
2015-07-25 01:53:53 -0500

    Adding unit tests.

--- emoji/core.py
+++ emoji/core.py
@@ -62,37 +62,21 @@ def demojize(string):
     """
 
     # via Martijn Pieters, http://stackoverflow.com/questions/26568722/remove-unicode-emoji-using-re-in-python
-    if PY2:
-        try:
-            # Wide UCS-4 build
-            pattern = re.compile(u'['
-                u'\U0001F300-\U0001F64F'
-                u'\U0001F680-\U0001F6FF'
-                u'\u2600-\u26FF\u2700-\u27BF]+', 
-                re.UNICODE)
-        except re.error:
-            # Narrow UCS-2 build
-            pattern = re.compile(u'('
-                u'\ud83c[\udf00-\udfff]|'
-                u'\ud83d[\udc00-\ude4f\ude80-\udeff]|'
-                u'[\u2600-\u26FF\u2700-\u27BF])+', 
-                re.UNICODE)
-    else:
-    # Python 3+ should handle all strings as unicode. Right?
-        try:
-            # Wide UCS-4 build
-            pattern = re.compile('['
-                '\U0001F300-\U0001F64F'
-                '\U0001F680-\U0001F6FF'
-                '\u2600-\u26FF\u2700-\u27BF]+', 
-                re.UNICODE)
-        except re.error:
-            # Narrow UCS-2 build
-            pattern = re.compile('('
-                '\ud83c[\udf00-\udfff]|'
-                '\ud83d[\udc00-\ude4f\ude80-\udeff]|'
-                '[\u2600-\u26FF\u2700-\u27BF])+', 
-                re.UNICODE)
+    try:
+        # Wide UCS-4 build
+        pattern = re.compile(u'['
+            u'\U0001F300-\U0001F64F'
+            u'\U0001F680-\U0001F6FF'
+            u'\u2600-\u26FF\u2700-\u27BF'
+            u']+', 
+            re.UNICODE)
+    except re.error:
+        # Narrow UCS-2 build
+        pattern = re.compile(u'('
+            u'\ud83c[\udf00-\udfff]|'
+            u'\ud83d[\udc00-\ude4f\ude80-\udeff]|'
+            u'[\u2600-\u26FF\u2700-\u27BF])+', 
+            re.UNICODE)
 
     def replace(match):
         return unicode_codes.UNICODE_EMOJI.get(match.group(0), match.group(0))
