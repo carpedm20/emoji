a2720edba571e5f50280f86b0a33f9389639f43e
sigoa <si@goat.si>
2018-01-05 19:29:59 +0100

    at runtime expect to see NO ICON - clarify
    at runtime expect to see NO ICON - clarify - at runtime expect to see TEXT instead. 
    
    Without this hint, the example is pretty much worthless for demonstration purposes.

--- example/example.py
+++ example/example.py
@@ -2,5 +2,5 @@
 import emoji
 
 print(emoji.emojize('Water! :water_wave:'))
-print(emoji.demojize(u'🌊')) # for Python 2.x
+print(emoji.demojize(u' at runtime expect to see a picture or icon but a regulate text instead     🌊')) # for Python 2.x
 # print(emoji.demojize('🌊')) # for Python 3.x
