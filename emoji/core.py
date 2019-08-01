895a89de315b3c7812696b4cdcf8223c0adf60d9
Harmon <Harmon758@gmail.com>
2019-08-01 14:17:22 -0500

    Escape backslashes in regex pattern

--- emoji/core.py
+++ emoji/core.py
@@ -40,7 +40,7 @@ def emojize(string, use_aliases=False, delimiters=(_DEFAULT_DELIMITER,_DEFAULT_D
         Python is fun 👍
     """
 
-    pattern = re.compile(u'(%s[a-zA-Z0-9\+\-_&.ô’Åéãíç()!#*]+%s)' % delimiters)
+    pattern = re.compile(u'(%s[a-zA-Z0-9\\+\\-_&.ô’Åéãíç()!#*]+%s)' % delimiters)
 
     def replace(match):
         mg = match.group(1).replace(delimiters[0], _DEFAULT_DELIMITER).replace(delimiters[1], _DEFAULT_DELIMITER)
