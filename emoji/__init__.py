32c5b173f78d72ae4aa34f07d3a3b3ac9b003785
Tahir Jalilov <tahir.jalilov@gmail.com>
2021-06-02 15:23:03 +0400

    v.1.3.0

--- emoji/__init__.py
+++ emoji/__init__.py
@@ -27,7 +27,7 @@ __all__ = [
     'EMOJI_ALIAS_UNICODE_ENGLISH', 'UNICODE_EMOJI_ALIAS_ENGLISH',
 ]
 
-__version__ = '1.2.1'
+__version__ = '1.3.0'
 __author__ = 'Taehoon Kim and Kevin Wurster'
 __email__ = 'carpedm20@gmail.com'
 # and wursterk@gmail.com
