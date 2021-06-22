b3aea088093232684e82ff18c44a3da09a026d6c
Tahir Jalilov <tahir.jalilov@gmail.com>
2021-06-22 19:31:04 +0400

    Added German to github pages

--- utils/gh-pages/generatePages.py
+++ utils/gh-pages/generatePages.py
@@ -29,7 +29,8 @@ languages = {
     "UNICODE_EMOJI_SPANISH": emoji.emojize(":Spain:"),
     "UNICODE_EMOJI_PORTUGUESE": emoji.emojize(":Portugal:"),
     "UNICODE_EMOJI_ITALIAN": emoji.emojize(":Italy:"),
-    "UNICODE_EMOJI_FRENCH" : emoji.emojize(":France:")
+    "UNICODE_EMOJI_FRENCH": emoji.emojize(":France:"),
+    "UNICODE_EMOJI_GERMAN": emoji.emojize(":Germany:")
 }
 language_args = {
     "UNICODE_EMOJI_ALIAS_ENGLISH": 'language="en", use_aliases=True'
