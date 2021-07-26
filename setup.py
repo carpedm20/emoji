09a07c73b9d6377c1ecb502d289acf5b4b744f21
Tahir Jalilov <tahir.jalilov@gmail.com>
2021-07-26 13:34:33 +0400

    Support of the python 2 has been removed

--- setup.py
+++ setup.py
@@ -38,8 +38,6 @@ setup(
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
