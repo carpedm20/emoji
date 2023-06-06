df32d07943ce69860abdea9467144db592dc0723
cvzi <cuzi@openmail.cc>
2023-06-06 18:55:54 +0200

    Add typing information

--- emoji/__init__.pyi
+++ emoji/__init__.pyi
@@ -7,22 +7,29 @@ from .core import (
     is_emoji as is_emoji,
     replace_emoji as replace_emoji,
     version as version,
+    analyze as analyze,
+    config as config,
 )
+from .tokenizer import (
+    Token as Token,
+    EmojiMatch as EmojiMatch,
+    EmojiMatchZWJ as EmojiMatchZWJ,
+    EmojiMatchZWJNonRGI as EmojiMatchZWJNonRGI,
+)
+
+
 from .unicode_codes import EMOJI_DATA, LANGUAGES, STATUS
 
 __all__ = [
-    "emojize",
-    "demojize",
-    "emoji_count",
-    "emoji_list",
-    "distinct_emoji_list",
-    "replace_emoji",
-    "version",
-    "is_emoji",
-    "EMOJI_DATA",
-    "STATUS",
-    "LANGUAGES",
+    # emoji.core
+    'emojize', 'demojize', 'analyze', 'config',
+    'emoji_list', 'distinct_emoji_list', 'emoji_count',
+    'replace_emoji', 'is_emoji', 'version',
+    'Token', 'EmojiMatch', 'EmojiMatchZWJ', 'EmojiMatchZWJNonRGI',
+    # emoji.unicode_codes
+    'EMOJI_DATA', 'STATUS', 'LANGUAGES',
 ]
+
 __version__: str
 __author__: str
 __email__: str
