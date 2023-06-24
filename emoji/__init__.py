791a7f227fc3a4e0fbf15a004c79b9e6a61053ae
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2023-06-24 14:48:48 +0200

    Add purely_emoji()

--- emoji/__init__.py
+++ emoji/__init__.py
@@ -19,7 +19,7 @@ __all__ = [
     # emoji.core
     'emojize', 'demojize', 'analyze', 'config',
     'emoji_list', 'distinct_emoji_list', 'emoji_count',
-    'replace_emoji', 'is_emoji', 'version',
+    'replace_emoji', 'is_emoji', 'purely_emoji', 'version',
     'Token', 'EmojiMatch', 'EmojiMatchZWJ', 'EmojiMatchZWJNonRGI',
     # emoji.unicode_codes
     'EMOJI_DATA', 'STATUS', 'LANGUAGES',
