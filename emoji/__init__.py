d97e3d431a0fffcfaba56c37d84dfcddac5e25a4
Tahir Jalilov <43037294+TahirJalilov@users.noreply.github.com>
2023-08-16 15:13:55 +0200

    Update __init__.py

--- emoji/__init__.py
+++ emoji/__init__.py
@@ -25,7 +25,7 @@ __all__ = [
     'EMOJI_DATA', 'STATUS', 'LANGUAGES',
 ]
 
-__version__ = '2.7.1'
+__version__ = '2.8.0'
 __author__ = 'Taehoon Kim, Kevin Wurster'
 __email__ = 'carpedm20@gmail.com'
 # and wursterk@gmail.com, tahir.jalilov@gmail.com
